d5078f7730cf5bda9ce063e8edc630de88824ee1
Ксения Доможилова <111085352+xushaha@users.noreply.github.com>
2023-05-13 09:20:03 +0000

    Sync LeetCode submission - Contains Duplicate (java)

--- problems/contains_duplicate/solution.java
+++ problems/contains_duplicate/solution.java
@@ -3,12 +3,16 @@ import java.util.Arrays;
 
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-
+//создаем хэш-сет
         HashSet hS = new HashSet();
+        //проходим по входному массиву
         for (int num : nums) {
+            //на каждом шаге проверяем, есть ли уже в хэш-сете текущий элемент массива
             if (hS.contains(num)) {
+                //если есть, возращаем тру
                 return true;
             }
+            //если нет, добавляем элемент в хэш-сет
             hS.add(num);
         }
 
